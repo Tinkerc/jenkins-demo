08a90c448f49d00a120789b9a4ce60742a52950d
Tinkerc <chenruoyun@126.com>
2019-04-09 18:55:41 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-
+PATH =/home/demo
+TIME =`date "+%Y%m%d%H%M"`
 echo 'hello startup.sh'
+mv ${PATH}/demo.jar demo-${TIME}-bak.jar
+cp target/demo.jar ${PATH}/demo.jar
+supervisorctl restart demo
